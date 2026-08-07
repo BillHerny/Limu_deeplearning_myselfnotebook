d600776155eca7e7cf6fde04d6d533ac07b86d73
BillHerry_Linux_Fate_Whispering <1694688731@qq.com>
2026-08-07 19:13:55 +0800

    添加了新版d2L中丢失的一些包，并利用这些包运行了mlp的前三节202608071910

--- myd2l/__init__.py
+++ myd2l/__init__.py
@@ -1,8 +1,14 @@
 
 from .compatibility import accuracy
 from .compatibility import evaluate_accuracy
+from .compatibility import train_epoch_ch3
+from .compatibility import train_ch3
+from .compatibility import Animator
 
 __all__ = [
     "accuracy",
     "evaluate_accuracy",
+    "train_epoch_ch3",
+    "train_ch3",
+    "Animator"
 ]
--- myd2l/compatibility.py
+++ myd2l/compatibility.py
@@ -1,7 +1,23 @@
 
 import torch
+import torch
+from d2l import torch as d2l
+from IPython import display
 
+class Accumulator:
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
 
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+      
 # def accuracy(y_hat, y):
 #     """统计预测正确的样本数量。"""
 #     if y_hat.ndim > 1 and y_hat.shape[1] > 1:
@@ -9,6 +25,8 @@ import torch
 
 #     comparison = y_hat.to(y.dtype) == y
 #     return float(comparison.to(torch.float32).sum())
+
+
 def accuracy(y_hat, y):
     """计算预测正确的数量"""
     if len(y_hat.shape) > 1 and y_hat.size(1) > 1:
@@ -53,3 +71,68 @@ def evaluate_accuracy(net, data_iter):  #@save
             # metric[0] += a （正确样本数累加）
             # metric[1] += b （总样本数累加）
     return metric[0] / metric[1]
+
+class Animator:
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）"""
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+#@save
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    """训练模型（定义见第3章）"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0, 1],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
